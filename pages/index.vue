44ee64d9c56ae5457b18eb48289214d01040a9e5
dorian <dorian.vidal@hetic.net>
2023-07-07 16:35:36 +0200

    add form on dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
       </div>
 
       <t-table
-        :headers="['Name', 'Slug', 'Numbers']"
+        :headers="['Nom', 'ID fonctionelle', 'Nombre étudiants']"
         :data="[
           {
             name: 'Alfonso Bribiesca',
@@ -107,50 +107,6 @@
                 :required="true"
                 v-model="form.lastname"
               />
-            </div>
-            <div class="grid px-8 md:px-12 md:grid-cols-2 md:gap-8">
-              <t-radio-group
-                :options="[
-                  { value: 'parent', text: 'Je suis parent' },
-                  { value: 'driver', text: 'Je suis chauffeur' },
-                ]"
-                name="radio"
-                required
-                v-model="form.profil"
-                :value="['checked']"
-              />
-            </div>
-            <div class="grid px-8 md:px-12 md:grid-cols-2 md:gap-8">
-              <base-input-text
-                type="mail"
-                namefor="mail"
-                label="Adresse e-mail"
-                placeholder=" "
-                pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
-                :required="true"
-                class="md:mr-8"
-                v-model="form.mail"
-              />
-              <base-input-text
-                type="tel"
-                namefor="phone"
-                label="Téléphone"
-                placeholder=" "
-                pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$"
-                :required="true"
-                v-model="form.phone"
-              />
-            </div>
-
-            <div class="grid grid-cols-1 px-8 md:px-12 md:gap-8">
-              <base-text-area
-                namefor="text"
-                id="text"
-                label="Votre message"
-                placeholder=" "
-                v-model="form.text"
-                required="true"
-              ></base-text-area>
             </div>
 
             <p
